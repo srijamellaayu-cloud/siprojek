bd203d936c7d2bb64a3207dc7e4ae57489423772
srijamellaayu-cloud <srijamellaayu@gmail.com>
2026-07-02 12:07:06 +0700

    Debug: add direct echo markers to index.php

--- api/index.php
+++ api/index.php
@@ -3,5 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Forward Vercel requests to Laravel's public/index.php
+echo "DEBUG: api/index.php is executing...<br>";
+
+if (!file_exists(__DIR__ . '/../public/index.php')) {
+    die("DEBUG ERROR: public/index.php not found!");
+}
+
+echo "DEBUG: public/index.php found. Attempting to require it...<br>";
 require __DIR__ . '/../public/index.php';
